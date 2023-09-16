718833db15861a6125ef451fc76dde5d58a27c8e
minkiko@gmail.com <minkiko@gmail.com>
2023-09-15 22:25:31 -0500

    creating a single thought by id on controller files

--- controllers/thoughtControlers.js
+++ controllers/thoughtControlers.js
@@ -12,3 +12,17 @@ module.exports = {
                 return res.status(500).json(err);
             });
     },
+
+    getSingleThought (req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with this id found' })
+                    : res.json(thought)
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
--- controllers/userController.js
+++ controllers/userController.js
@@ -11,4 +11,18 @@ module.exports = {
                 console.log(err);
                 return res.status(500).json(err);
             });
-    },v
+    },
+
+    getSingleThought (req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with this id found' })
+                    : res.json(thought)
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },

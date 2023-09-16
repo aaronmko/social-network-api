10d62a8d4c8469eccb059a8dbcf074678fa5c5b0
minkiko@gmail.com <minkiko@gmail.com>
2023-09-15 22:29:08 -0500

    removing a friend on controller file

--- controllers/userController.js
+++ controllers/userController.js
@@ -94,3 +94,24 @@ module.exports = {
                 return res.status(500).json(err)
             });
     },
+
+    removeFriend (req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this id found' })
+                    : res.json({
+                        updatedUser: user,
+                        message: 'Friend removed'
+                    })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+};

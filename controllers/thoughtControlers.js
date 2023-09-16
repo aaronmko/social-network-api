ba5cc20733490a9b68dfe2eb3459bb82d1b89942
minkiko@gmail.com <minkiko@gmail.com>
2023-09-15 22:24:34 -0500

    initial creation of MVC

--- controllers/thoughtControlers.js
+++ controllers/thoughtControlers.js
@@ -1,1 +1,14 @@
 const { Thought, User } = require('../models');
+
+module.exports = {
+    // Get all thoughts
+    getThoughts (req, res) {
+        Thought.find()
+            .then((thoughts) => {
+                return res.json(thoughts);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,14 @@
 const { User, Thought } = require('../models');
+
+module.exports = {
+    // Get all users
+    getUsers (req, res) {
+        User.find()
+            .then((users) => {
+                return res.json(users);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },v
